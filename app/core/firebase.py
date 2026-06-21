7689e6daa4a09279ae8ee282e3c1ce42a168a696
ronald <rebulgame@gmail.com>
2026-06-21 16:55:59 -0300

    feat: update Firebase initialization to use credentials from settings

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,20 +1,12 @@
-from pathlib import Path
-
 import firebase_admin
-
 from firebase_admin import credentials
 
-
-SERVICE_ACCOUNT_PATH = (
-    Path(__file__).resolve().parents[1]
-    / "secrets"
-    / "cinepulseapp-f6398-firebase-adminsdk-fbsvc-be60bfa73e.json"
-)
+from app.core.config import settings
 
 
 def initialize_firebase():
     try:
         return firebase_admin.get_app()
     except ValueError:
-        cred = credentials.Certificate(str(SERVICE_ACCOUNT_PATH))
+        cred = credentials.Certificate(settings.firebase_credentials)
         return firebase_admin.initialize_app(cred)
